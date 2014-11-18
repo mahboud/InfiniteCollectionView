5cb7a58b361e4f5d44c863cff111f577ccdfa987
Mahboud Zabetian <mahboud@silentcircle.com>
2014-11-18 02:49:56 -0800

    Initial Commit

---
+++ GFExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GFExercise
+//
+//  Created by mahboud on 11/18/14.
+//  Copyright (c) 2014 mahboud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GFExercise/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GFExercise
+//
+//  Created by mahboud on 11/18/14.
+//  Copyright (c) 2014 mahboud. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end

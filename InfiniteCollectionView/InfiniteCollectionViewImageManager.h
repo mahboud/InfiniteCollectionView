c82343f62b8eba5b9dbb5d57b561180a671208b2
Mahboud Zabetian <mahboud@optimized.com>
2015-01-01 22:39:46 -0800

    protocol added to interface to clear compiler warning

--- InfiniteCollectionView/InfiniteCollectionViewImageManager.h
+++ InfiniteCollectionView/InfiniteCollectionViewImageManager.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface InfiniteCollectionViewImageManager : NSObject {
+@interface InfiniteCollectionViewImageManager : NSObject <NSURLSessionDelegate> {
 
 }
 
